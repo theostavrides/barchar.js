006688259f7853b2a64307ff5b4f3cc08bd39047
Theo Stavrides <theostavrides@gmail.com>
2019-03-21 18:32:39 -0700

    add drawLines() function

--- script.js
+++ script.js
@@ -3,22 +3,8 @@ function drawBarChart(data, options, $element) {
     $element = $(element);
   }
 
-  let n = data.length;
-  let biggestBarInUnits = Math.max(...data);
-
-  function drawBars() {
-    let barWidth = (options.css.width - options.spacing * (n + 1))/n;
-    let x = 0;
-    for (let bar of data) {
-      let barHeight = getBarHeight(bar);
-      x += options.spacing;
-      makeBarElement(x, options.css.height - barHeight, barWidth, barHeight);
-      x += barWidth;
-    }
-  }
-
   function getBarHeight(bar) {
-    let ratio = bar / biggestBarInUnits;
+    let ratio = bar / Math.max(...data);
     let paddingTop = options.paddingTopPercent * options.css.height / 100;
     let height = (options.css.height - paddingTop) * ratio;
     return height;
@@ -30,26 +16,60 @@ function drawBarChart(data, options, $element) {
                            top: y,
                            width: width,
                            height: height,
-                           'background-color': 'red'
+                           'background-color': 'black'
                           }
                      };
     let $bar = $("<div></div>", attributes);
     $element.append($bar)
+  }
 
+  function drawBars() {
+    let n = data.length;
+    let barWidth = (options.css.width - options.spacing * (n + 1))/n;
+    let x = 0;
+    for (let bar of data) {
+      let barHeight = getBarHeight(bar);
+      x += options.spacing;
+      makeBarElement(x, options.css.height - barHeight, barWidth, barHeight);
+      x += barWidth;
+    }
+  }
+
+  function drawLines(){
+    let maxInput = Math.max(...data);
+    let paddingTop = options.paddingTopPercent * options.css.height / 100;
+    let maxBarHeight = options.css.height - paddingTop;
+    let attributes = {css: {position: 'absolute',
+                              height: 1,
+                              width: options.css.width,
+                              bottom: maxBarHeight,
+                             'background-color': 'blue'
+                             }
+                       }
+
+    for (let i = 0; i < data.length; i++) {
+
+
+    }
+    let $line = $('<div></div>', attributes);
+    $element.append($line)
   }
 
   $element.css(options.css);
+  drawLines();
   drawBars();
+
 }
 
 
 
 let options =  {css: {height: 500,
                       width: 500,
-                     'background-color': 'blue',
+                     'background-color': 'lightgrey',
                       position: 'relative'},
-                spacing: 50,
-                paddingTopPercent: 4
+                spacing: 20,
+                paddingTopPercent: 4,
+                lineNumber: 8
               }
 
-drawBarChart([12,2,30,45,15,26,50], options, $("#chart"))
+drawBarChart([192, 21,30,39,48,57,66], options, $("#chart"))
